b5ba4c16ad53ddfcaf010d990addc5a8d86d97a5
Carien <ccaljouw@student.codam.nl>
2023-09-12 13:46:53 +0200

    simple color gradiant example

--- src/main.c
+++ src/main.c
@@ -6,13 +6,96 @@
 /*   By: ccaljouw <ccaljouw@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/12 10:11:39 by ccaljouw      #+#    #+#                 */
-/*   Updated: 2023/09/12 10:12:14 by ccaljouw      ########   odam.nl         */
+/*   Updated: 2023/09/12 12:55:27 by ccaljouw      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int argc, char **argv)
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <MLX42.h>
+#include <math.h>
+
+#define WIDTH 1024
+#define HEIGHT 1024
+
+static mlx_image_t* image;
+
+// -----------------------------------------------------------------------------
+
+int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
 {
+    return (r << 24 | g << 16 | b << 8 | a);
+}
+
+void ft_randomize(void* param)
+{
+	double R;
+	double G;
+	(void)param;
+	for (uint32_t x = 0; x < image->width; ++x)
+	{
+		for (uint32_t y = 0; y < image->height; ++y)
+		{
+			R = round(((double)x / (double)image->width) * 255);
+			G = round(((double)y / (double)image->height) * 255);
+			uint32_t color = ft_pixel(
+				(uint32_t)R, 			// R
+				(uint32_t)G, 			// G
+				0,					   	// B
+				255						// A
+			);
+			mlx_put_pixel(image, x, y, color);
+		}
+	}
+}
+
+void ft_hook(void* param)
+{
+	mlx_t* mlx = param;
+
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_UP))
+		image->instances[0].y -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
+		image->instances[0].y += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
+		image->instances[0].x -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+		image->instances[0].x += 5;
+}
+
+// -----------------------------------------------------------------------------
+
+int32_t main(int32_t argc, const char* argv[])
+{
+	mlx_t* mlx;
+
 	(void)argc;
 	(void)argv;
-	return (0);
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "COLOR GRADIENT", true)))
+	{
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	if (!(image = mlx_new_image(mlx, 256, 256)))
+	{
+		mlx_close_window(mlx);
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
+	{
+		mlx_close_window(mlx);
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	
+	mlx_loop_hook(mlx, ft_randomize, mlx);
+	mlx_loop_hook(mlx, ft_hook, mlx);
+
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
 }
