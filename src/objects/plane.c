e37dc8eed13c30e7c08d2f1bcaceca83d48cc85f
post@albertvanandel.nl <you@example.com>
2023-09-22 21:41:01 +0200

    fixed plane normal interpretation

--- src/objects/plane.c
+++ src/objects/plane.c
@@ -1,16 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   plane.c                                            :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: ccaljouw <ccaljouw@student.codam.nl>         +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2023/09/20 11:14:41 by ccaljouw      #+#    #+#                 */
-/*   Updated: 2023/09/21 10:48:44 by ccaljouw      ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   plane.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albertvanandel <albertvanandel@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/20 11:14:41 by ccaljouw          #+#    #+#             */
+/*   Updated: 2023/09/22 21:36:54 by albertvanan      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/miniRT.h"
+void	print_vector(t_xyz vector)
+{
+	printf("[%.3f, %.3f, %.3f]\n", vector.x, vector.y, vector.z);
+}
 
 
 int	test_plane(t_px ray, t_object plane, float *hit_dist)
@@ -19,15 +23,12 @@ int	test_plane(t_px ray, t_object plane, float *hit_dist)
 	t_xyz	diff_ray0_plane0;
 
 	denominator = v_dot(plane.vNormal, ray.direction);
-	if (denominator > EPSILON)
+	if (fabsf(denominator) > EPSILON)
 	{
-		diff_ray0_plane0 = v_subtract( plane.pOrigin, ray.cam_origin);
-		*hit_dist = v_dot(diff_ray0_plane0, plane.vNormal) / denominator;
-		if (*hit_dist >= 0)
-		{
-			// get_plane_surface_data(plane, ray);
+		diff_ray0_plane0 = v_subtract(plane.pOrigin, ray.cam_origin);
+		*hit_dist = v_dot(plane.vNormal, diff_ray0_plane0) / denominator;
+		if (*hit_dist > 0)
 			return (1);
-		}
 	}
 	return (0);
 }
@@ -37,7 +38,7 @@ int	get_plane_surface_data(t_object plane, t_px px, t_scene scene)
 {
 	float	facing_ratio;
 
-	facing_ratio = v_dot(v_normalize(plane.vNormal), v_normalize(px.direction));
+	facing_ratio = fabsf(v_dot(v_normalize(plane.vNormal), v_normalize(px.direction)));
 	return \
 	((int)(plane.rgb[0] * scene.ambient->rgb_ratio[0] * facing_ratio) << 24 \
 	| (int)(plane.rgb[1] * scene.ambient->rgb_ratio[1] * facing_ratio) << 16 \
